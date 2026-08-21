adb6b978e5fc10d188c44aa85477195bdba44622
Gerardo Cendejas Mendoza <gc597@cbsuenbody01.biohpc.cornell.edu>
2026-08-21 11:47:12 -0400

    env: Change genomics_general github repo

--- workflow/envs/genomics_general.post-deploy.sh
+++ workflow/envs/genomics_general.post-deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-GENOMICS_GENERAL_COMMIT="39243440565d138f3eedd1f99b62916254706f71"
+GENOMICS_GENERAL_COMMIT="3d10f8e9570e500524bdcc8082dd1dcb08b6946f"
 SRC="${CONDA_PREFIX}/opt/genomics_general-${GENOMICS_GENERAL_COMMIT:0:7}"
 
 mkdir -p "${SRC}"
-curl -fsSL "https://codeload.github.com/simonhmartin/genomics_general/tar.gz/${GENOMICS_GENERAL_COMMIT}" \
+curl -fsSL "https://codeload.github.com/GerardoCendejas/genomics_general/tar.gz/${GENOMICS_GENERAL_COMMIT}" \
   | tar -xz --strip-components=1 -C "${SRC}"
 
 chmod +x "${SRC}/phylo/phyml_sliding_windows.py"
